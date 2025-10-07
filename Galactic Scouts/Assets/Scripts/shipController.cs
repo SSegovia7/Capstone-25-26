e1f3a86ca06bffa516cbf2ae2cfd7ce5d764f1d1
sprigdonut <chenk27@uci.edu>
2025-10-06 23:42:10 -0700

    ship scene

---
+++ Galactic Scouts/Assets/Scripts/shipController.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shipController : MonoBehaviour
+{
+    private Rigidbody2D _rb2d;
+    private SpriteRenderer _sprite;
+}
