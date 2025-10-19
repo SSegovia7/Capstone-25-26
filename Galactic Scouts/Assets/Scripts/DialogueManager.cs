6a9bfe730314a85ba3cb635b3249b37083f3118a
Jack Patschull <brennanpatschull@gmail.com>
2025-10-18 21:15:34 -0700

    Finished Dialogue

--- Galactic Scouts/Assets/Scripts/DialogueManager.cs
+++ Galactic Scouts/Assets/Scripts/DialogueManager.cs
@@ -16,23 +16,57 @@ public class DialogueManager : MonoBehaviour
     private TextMeshProUGUI characterNameText;
     private Image characterPortraitImage;
 
+    //Things here have to do with managing the dialogue
     public DialogueData[] currentDialogueData;
-    private float typingSpeed = 0.05f;
+    private bool advanceCurrentDialogue = true;
+    private int currentDialogueStep = 0;
 
+    private float typingSpeed = 0.05f;
+    private float timeAfterTextCompletes = 5f;
     private void Start()
     {
+        //Assign components to variables
         dialogueBoxText = dialogueBox.GetComponent<TextMeshProUGUI>();
         characterNameText = characterName.GetComponent<TextMeshProUGUI>();
         characterPortraitImage = characterPortrait.GetComponent<Image>();
     }
-    //Function updates the dialogue box
-    public IEnumerator updateTextBox(DialogueData dialogue)
+    private void FixedUpdate()
     {
-        //Goes for every letter in the string
-        for (int x = 0; x <= dialogue.dialogue.Length; x++)
+        //Advances dialogue automatically if there is dialogue data to run
+        if (currentDialogueData != null && advanceCurrentDialogue)
+        {
+            if (currentDialogueStep <= currentDialogueData.Length)
+            {
+                UpdateDialogue(currentDialogueData[currentDialogueStep]);
+            }
+            else
+            {
+                //If at the end of the list, removes data and resets counter
+                currentDialogueStep = 0;
+                currentDialogueData = null;
+            }
+        }
+    }
+    //Updates the name and image then calls Couroutine that updates the text
+    public void UpdateDialogue(DialogueData dialogue)
+    {
+        advanceCurrentDialogue = false;
+        characterNameText.text = dialogue.speakerName;
+        dialogueBoxText.text = null;
+        characterPortraitImage.sprite = dialogue.speakerPortrait;
+        StartCoroutine(UpdateTextBox(dialogue));
+    }
+    //Function updates the dialogue box to create the typewriter effect
+    private IEnumerator UpdateTextBox(DialogueData dialogue)
+    {
+        //Goes through every letter in the string
+        for (int x = 0; x < dialogue.dialogue.Length; x++)
         {
             dialogueBoxText.text += dialogue.dialogue[x];
             yield return new WaitForSeconds(typingSpeed);
         }
+        yield return new WaitForSeconds(timeAfterTextCompletes);
+        currentDialogueStep += 1;
+        advanceCurrentDialogue = true;
     }
 }
