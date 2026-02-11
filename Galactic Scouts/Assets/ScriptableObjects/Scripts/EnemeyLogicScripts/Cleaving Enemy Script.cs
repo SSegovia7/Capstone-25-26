34729cc88090668ac451567610e4294881683fb7
Jack Patschull <brennanpatschull@gmail.com>
2026-02-10 18:37:03 -0800

    I think I have the logic for the enemy done

--- Galactic Scouts/Assets/ScriptableObjects/Scripts/EnemeyLogicScripts/Cleaving Enemy Script.cs
+++ Galactic Scouts/Assets/ScriptableObjects/Scripts/EnemeyLogicScripts/Cleaving Enemy Script.cs
@@ -6,20 +6,24 @@ using UnityEngine;
 
 public class CleavingEnemyScript : MonoBehaviour
 {
+    [SerializeField] private float speed;
     [SerializeField] private GameObject warning;
     [SerializeField] private float warningActiveTime;
+    private Rigidbody2D rb;
     // Start is called before the first frame update
     //Warning sign spawns before actual enemy
     void Awake()
     {
+        rb = GetComponent<Rigidbody2D>();
         StartCoroutine("WarningSign");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
+    //Also moves enemy because I'm silly :3
     private IEnumerator WarningSign()
     {
         //Spawns warning on the correct side of the map
@@ -27,10 +31,12 @@ public class CleavingEnemyScript : MonoBehaviour
         if (gameObject.transform.position.x < 0)
         {
             warningSpawnPosition = new Vector3(-6f, gameObject.transform.position.y, gameObject.transform.position.z);
+            rb.velocity = new Vector3(speed, 0, 0);
         }
         else
         {
             warningSpawnPosition = new Vector3(6f, gameObject.transform.position.y, gameObject.transform.position.z);
+            rb.velocity = new Vector3(-speed, 0, 0);
         }
         GameObject wObject = Instantiate(warning);
         wObject.transform.position = warningSpawnPosition;
